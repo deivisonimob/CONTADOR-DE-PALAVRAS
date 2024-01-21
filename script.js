9259142e3619f1b1b2a630b5e76bd7c7e65c97d6
deivisonimob <149898447+deivisonimob@users.noreply.github.com>
2024-01-20 23:21:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ btnEnviar.addEventListener("click", function() {
     resultado.innerHTML += "Repetições: ";
     
     const palavras = textoEnviado.split(" ");
-    const contagemPalavras = {};  // Usaremos um objeto para armazenar as contagens de palavras
+    const contagemPalavras = {}; 
     
     for (let i = 0; i < palavras.length; i++) {
         const palavra = palavras[i];
         
-        // Verificamos se a palavra já existe no objeto de contagem
+        
         if (contagemPalavras[palavra] === undefined) {
             contagemPalavras[palavra] = 1;
         } else {
@@ -23,12 +23,12 @@ btnEnviar.addEventListener("click", function() {
         }
     }
     
-    // Exibimos as contagens no resultado
+    
     for (const palavra in contagemPalavras) {
         resultado.innerHTML += palavra + ": esse(a) corretor(a) fez " + contagemPalavras[palavra] + " período<br>";
     }
     
-    // Calculamos e exibimos as repetições totais
+    
     let repeticoesTotais = 0;
     for (const palavra in contagemPalavras) {
         repeticoesTotais += contagemPalavras[palavra];
